b058cdf5bbbc57340e854791b975c19764502166
md4Studios <md4official@gmail.com>
2022-03-20 20:21:20 +0100

    Update

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -10,5 +10,11 @@ export default [
     name: "ThreeWhi",
     userId: "ThreeWhi",
     avatar: "https://avatars0.githubusercontent.com/u/70823682?s=400&u=7ee96b166eba6a3552aef0f9496271c5b13adb7a&v=4"
+  },
+  {
+    id: 3,
+    name: "Dr4aKy",
+    userId: "Dr4aKy",
+    avatar: "https://avatars0.githubusercontent.com/u/95684256?s=400&u=7ee96b166eba6a3552aef0f9496271c5b13adb7a&v=4"
   }
 ]
